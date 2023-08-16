868f0d4a03a1871f7a595a3641de621680c4b1de
Ziad Elsayed <93676730+ziadelsayed2004@users.noreply.github.com>
2023-08-16 14:22:00 +0300

    Add files via upload

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $formErrors[] = "Name must be more than 3 characters";
     }
 
-    if (strlen($mail) <= 0) {
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
         $formErrors[] = "The email is required field !";
     }
 
@@ -23,12 +23,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $headers = "From: " .$mail . "/n/r";
 
-    if (empty($formErrors)) {
-
-        mail("ziadelsayed2004@gmail.com", "Website Conatact", $msg, $headers );
-        
-        echo "Your Email Has Sent Succesfully";
-    }
-
  }
+
+ if (empty($formErrors)) {
+
+    mail("ziadelsayed2004@gmail.com", "Website Conatact", $msg, $headers );
+    
+    echo "Your Email Has Sent Succesfully";
+}
 ?>
